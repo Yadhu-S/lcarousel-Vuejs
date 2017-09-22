4d48dffc9e75509dcf91ec9568d5bd3af96b4ef0
Yadhu Sasidharan <yadhu@Yadhus-MacBook-Pro.local>
2017-09-23 02:02:42 +0530

    lots of fixes almost usable now

--- src/components/Slides.vue
+++ src/components/Slides.vue
@@ -2,18 +2,22 @@
 <div>
   <div :style="`width:${topImgSize}px; height:${topImgSize}px;`" class="topPrev">
     <div class="slideImg" :style="`transform:translateX(${getOffsetTop}px);`">
-      <img v-for="slide in slides" :key="slide.id" class="bigImgs" :src="slide.src">
+      <div v-for="slide in slides" :key="slide.id" class="bigImgCont">
+        <img class="bigImgs" :src="slide.src">
+      </div>
     </div>
   </div>
+  <i @click="moveLeft" class="fa fa-angle-left" id="leftArrow" aria-hidden="true"></i>
+  <i @click="moveRight"class="fa fa-angle-right" id="rightArrow" aria-hidden="true"></i>
   <div class="bottomPrev">
     <div class="slideImg" :style="`transform:translateX(${getOffset}px);`" >
-      <img v-for="(slide,index) in slides" @mouseover="prevPic(index)" 
-      @mouseout="away" @click="fixImg(index)"
-      :key="slide.id" class="smallImgs" :src="slide.src">
+      <div v-for="(slide,index) in slides" :key="slide.id" class="smallImgCont">
+        <img @mouseover="prevPic(index)" 
+        @mouseout="away" @click="fixImg(index)"
+        class="smallImgs" :src="slide.src">
+      </div>
     </div>
   </div>
-  <button @click.prevent="moveLeft">Left</button>
-  <button @click.prevent="moveRight">Right</button>
 </div>
 </template>
 
@@ -26,13 +30,13 @@ export default {
       temp: 0,
       mvLft: false,
       slides: [
-        {src: '../../static/1.jpg'},
-        {src: '../../static/2.jpg'},
-        {src: '../../static/3.jpg'},
-        {src: '../../static/4.jpg'},
-        {src: '../../static/5.jpg'},
-        {src: '../../static/6.jpg'},
-        {src: '../../static/7.jpg'}
+        {src: '../../../static/1.jpg'},
+        {src: '../../../static/2.jpg'},
+        {src: '../../../static/3.jpg'},
+        {src: '../../../static/4.jpg'},
+        {src: '../../../static/5.jpg'},
+        {src: '../../../static/6.jpg'},
+        {src: '../../../static/7.jpg'}
       ]
     }
   },
@@ -48,7 +52,6 @@ export default {
         this.mvLft = false
         return this.offset
       }
-      console.log(this.mvLft)
     },
     getOffsetTop () {
       return this.offsetTop
@@ -93,25 +96,62 @@ export default {
 </script>
 
 <style scoped>
-.bottomPrev,.topPrev{
-  width:300px;
-  height: 100px;
-  background-color: gray;
-  overflow: hidden;
+.smallImgCont,.bigImgCont{
+  background-color: white;
+  display: inline-block;
+  margin: 0px;
+  width: 100px;
+  height: 80px;
+  line-height: 77px;
+  text-align: center;
 }
-.smallImgs,.bigImgs{
-  width:100px;
-  height: 100px;
-  flex: 1 0 auto;
+#leftArrow,#rightArrow{
+  cursor: pointer;
+  margin-left: -14px;
+  margin-top: -176px;
+  position: absolute;
+  font-size: 44px;
 }
-.bigImgs {
+#rightArrow{
+  float: right;
+  margin-left: 292px
+}
+.bigImgCont{
   width: 300px;
   height: 300px;
 }
+.smallImgCont:hover {
+  box-shadow: 4px 3px 6px 0px rgba(244, 97, 80, 0.85);
+}
+.smallImgCont:active {
+  box-shadow: 4px 3px 6px 0px rgba(244, 97, 80, 0.85);
+}
+.bottomPrev,.topPrev{
+  width:300px;
+  height: 100px;
+  overflow: hidden;
+}
+.smallImgs{
+  width:100px;
+  height: auto;
+}
+.bigImgs {
+  max-width:100%;
+  max-height:100%;
+  vertical-align: middle;
+}
 .smallImgs{
   cursor: pointer;
+  max-width:98%;
+  max-height:98%;
+  vertical-align: middle;
+}
+.smallImgs:hover{
+  max-width: 100%;
+  max-height: 100%;
 }
 .slideImg{
-  display: flex;
+  display: inline-block;
+  white-space: nowrap;
 }
 </style>
